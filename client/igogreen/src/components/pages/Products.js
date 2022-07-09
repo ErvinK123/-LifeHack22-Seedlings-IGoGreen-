3b37b9c78c4c24bb04e49b5ef78466a48cbadc20
Wei <chongweikang5@gmail.com>
2022-07-09 19:15:10 +0800

    Template

---
+++ client/igogreen/src/components/pages/Products.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Products() {
+  return <>
+           <div><p> Hello there, you have <strong>5</strong> points.</p></div>
+           </>;
+}
