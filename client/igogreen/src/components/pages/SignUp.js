53a9f73b88f63a960eeb8d55d6f716ab07124fe1
ErvinK123 <ervinkin@yahoo.com.sg>
2022-07-10 04:00:53 +0800

    Sign-up page
    -

--- client/igogreen/src/components/pages/SignUp.js
+++ client/igogreen/src/components/pages/SignUp.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import '../../App.css';
 
+document.body.style.backgroundColor = "#D9F8C4";
 export default function SignUp() {
-  return <h1 className='sign-up'>Sign up page</h1>;
+  return (<div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}> 
+	     <form>
+              <label for="username">Username:</label><br />
+                <input type="text" id="username" name="username" /><br />
+              <label for="pwd">Password:</label><br />
+                <input type="password" id="pwd" name="pwd" /><br />
+	      <button className='btn btn-primary bm-3 mt-3 mb-1' >Sign-up</button><br /> 
+              <p>Already have an account? <a href="">Login</a></p>
+	  </form> 	  </div>);
 }
