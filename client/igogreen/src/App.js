b4fb870aff3f23e8d5b8a8d64958edd8ebf92471
Donovan <donovanlee_25@hotmail.com>
2022-07-09 22:31:51 +0800

    Added tables

--- client/igogreen/src/App.js
+++ client/igogreen/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
@@ -9,13 +9,14 @@ import ContactUs from './components/pages/ContactUs';
 import SignUp from './components/pages/SignUp';
 import Marketing from './components/pages/Marketing';
 import Consulting from './components/pages/Consulting';
+import ListItems from "./components/ListItems";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/' exact component={Home} />
+        <Route path='/' exact component={ListItems} />
         <Route path='/services' component={Services} />
         <Route path='/products' component={Products} />
         <Route path='/contact-us' component={ContactUs} />
