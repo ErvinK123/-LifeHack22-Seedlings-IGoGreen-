b6bc13b86484267185d0011c360da7737881990c
Donovan <donovanlee_25@hotmail.com>
2022-07-10 05:26:19 +0800

    Updates

---
+++ precache-manifest.cf199e460d69ce415ac11ee44017daee.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d6c10f286b222d32398d8308c74c73",
+    "url": "/LifeHack22_SeedLings_IGoGreen/index.html"
+  },
+  {
+    "revision": "4723f76b9b7f1918edf1",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/css/2.c1c45ef1.chunk.css"
+  },
+  {
+    "revision": "3b0b14fdb3bd4adfb3a6",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/css/main.011fb5d7.chunk.css"
+  },
+  {
+    "revision": "4723f76b9b7f1918edf1",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/js/2.b7ebfb9d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/js/2.b7ebfb9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0b14fdb3bd4adfb3a6",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/js/main.e483cc32.chunk.js"
+  },
+  {
+    "revision": "b8ede3027767476de539",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/js/runtime-main.c977f80f.js"
+  },
+  {
+    "revision": "ec0ae724e2039a6e577d10a908cf6254",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/media/blue.ec0ae724.jpg"
+  },
+  {
+    "revision": "5c12e3f20acbe66500c824b9bce520d3",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/media/donovan.5c12e3f2.jpg"
+  },
+  {
+    "revision": "0962831cd7d0a82c726332392fa92ad5",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/media/ervin.0962831c.jpg"
+  },
+  {
+    "revision": "7bca4587bd61bcdc0843365774bdaecd",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/media/face4.7bca4587.jpg"
+  },
+  {
+    "revision": "b080eb658ce7dd71bc15e53a9dddfd33",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/media/green.b080eb65.jpg"
+  },
+  {
+    "revision": "e05b371749cf610c8cb97453264915bb",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/media/kok.e05b3717.jpg"
+  },
+  {
+    "revision": "96dd8880e5b1a9bfb8b16651b7ee56b7",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/media/red.96dd8880.jpg"
+  },
+  {
+    "revision": "b2b8b8fc84c3f8dc3e7f5745a0a8c7ad",
+    "url": "/LifeHack22_SeedLings_IGoGreen/static/media/yellow.b2b8b8fc.jpg"
+  }
+]);
